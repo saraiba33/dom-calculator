4f55e0a920fb680c26d2786b8f70de634c714ca4
Sara Ibarra <saraiba33@gmail.com>
2022-04-08 16:13:35 -0600

    Change if else to switch

--- app.js
+++ app.js
@@ -1,42 +1,74 @@
 const operators = document.querySelectorAll(".operator")
 const screen = document.querySelector("#screen")
 const clearButton = document.querySelector("#clear")
-const allSpans = document.querySelectorAll("Span");
-
-function addScreenContent() {
-    screen.textContent += event.target.innerText
-}
+const allNumbers = document.querySelectorAll("span:not(.operator)");
 
 function errorMessage() {
     screen.textContent = "Error"
 }
 
 
-const allNumbers = [allSpans[15], allSpans[11], allSpans[12], allSpans[13], allSpans[7], allSpans[8], allSpans[9], allSpans[3], allSpans[4], allSpans[5]];
 allNumbers.forEach(number => {
     number.addEventListener("click", (event) => {
-        if (screen.textContent === "Error") {
-            return;
-        } else {
-            addScreenContent()
+        switch (screen.textContent) {
+            case "Error":
+                errorMessage()
+                break;
+            default:
+                screen.textContent += event.target.textContent
         }
     })
 })
 
+// if (screen.textContent === "Error") {
+//     return;
+// } else {
+//     screen.textContent += event.target.textContent
+// }
+
+// operators.forEach(operator => {
+//     operator.addEventListener("click", (event) => {
+//         if (operator.textContent === "=") {
+//             screen.textContent = eval(screen.textContent)
+//         } else if (screen.textContent === "") {
+//             errorMessage()
+//         } else if (screen.textContent === "Error") {
+//             return;
+//         } else if (operator.textContent === "x") {
+//             screen.textContent += "*"
+//         } else if (operator.textContent === "÷") {
+//             screen.textContent += "/"
+//         } else {
+//             screen.textContent += event.target.innerText
+//         }
+//     })
+// })
+
 operators.forEach(operator => {
     operator.addEventListener("click", (event) => {
-        if (operator.textContent === "=") {
-            screen.textContent = eval(screen.textContent)
-        } else if (screen.textContent === "") {
-            errorMessage()
-        } else if (screen.textContent === "Error") {
-            return;
-        } else if (operator.textContent === "x") {
-            screen.textContent += "*"
-        } else if (operator.textContent === "÷") {
-            screen.textContent += "/"
-        } else {
-            addScreenContent()
+        switch (operator.textContent) {
+            case "=":
+                screen.textContent = eval(screen.textContent)
+                break;
+            case "x":
+                screen.textContent += "*"
+                break;
+            case "÷":
+                screen.textContent += "/"
+                break;
+
+            default:
+                screen.textContent += event.target.textContent
+        }
+        switch (screen.textContent) {
+            case "":
+                errorMessage()
+            case "Infinity":
+                errorMessage()
+                break;
+            case "Error":
+                errorMessage()
+                break;
         }
     })
 })
@@ -44,10 +76,6 @@ operators.forEach(operator => {
 clearButton.addEventListener("click", () => {
     screen.textContent = ""
 })
-
-
-
-
 
 
 
