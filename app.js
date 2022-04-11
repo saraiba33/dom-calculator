e1533fa6046cb0ffdb053bb495082058be0f2bd9
Sara Ibarra <saraiba33@gmail.com>
2022-04-11 15:09:36 -0600

    Add final solution

--- app.js
+++ app.js
@@ -3,97 +3,37 @@ const screen = document.querySelector("#screen")
 const clearButton = document.querySelector("#clear")
 const allNumbers = document.querySelectorAll("span:not(.operator)");
 
-function errorMessage() {
-    screen.textContent = "Error"
-}
-
 
 allNumbers.forEach(number => {
-    number.addEventListener("click", (event) => {
-        switch (screen.textContent) {
-            case "Error":
-                errorMessage()
-                break;
-            default:
-                screen.textContent += event.target.textContent
+    number.addEventListener("click", event => {
+        if (screen.textContent === "Error") {
+            screen.textContent = "Error"
+        } else {
+            screen.textContent += event.target.textContent
         }
     })
 })
 
-// if (screen.textContent === "Error") {
-//     return;
-// } else {
-//     screen.textContent += event.target.textContent
-// }
-
-// operators.forEach(operator => {
-//     operator.addEventListener("click", (event) => {
-//         if (operator.textContent === "=") {
-//             screen.textContent = eval(screen.textContent)
-//         } else if (screen.textContent === "") {
-//             errorMessage()
-//         } else if (screen.textContent === "Error") {
-//             return;
-//         } else if (operator.textContent === "x") {
-//             screen.textContent += "*"
-//         } else if (operator.textContent === "÷") {
-//             screen.textContent += "/"
-//         } else {
-//             screen.textContent += event.target.innerText
-//         }
-//     })
-// })
-
 operators.forEach(operator => {
     operator.addEventListener("click", (event) => {
-        switch (operator.textContent) {
-            case "=":
-                screen.textContent = eval(screen.textContent)
-                break;
-            case "x":
-                screen.textContent += "*"
-                break;
-            case "÷":
-                screen.textContent += "/"
-                break;
+        if (operator.textContent === "=") {
+            const result = eval(screen.textContent)
+            screen.textContent = result === Infinity || isNaN(result) ? "Error" : result
+        } else if (screen.textContent === "") {
+            screen.textContent = "Error"
+        } else if (screen.textContent === "Error") {
+            screen.textContent = "Error"
+        } else if (operator.textContent === "x") {
+            screen.textContent += "*"
+        } else if (operator.textContent === "÷") {
+            screen.textContent += "/"
+        } else {
+            screen.textContent += event.target.innerText
+        }
 
-            default:
-                screen.textContent += event.target.textContent
-        }
-        switch (screen.textContent) {
-            case "":
-                errorMessage()
-            case "Infinity":
-                errorMessage()
-                break;
-            case "Error":
-                errorMessage()
-                break;
-        }
     })
 })
 
 clearButton.addEventListener("click", () => {
     screen.textContent = ""
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// clearButton.addEventListener('keydown', event => {
-//     if (event.key === "Escape") {
-//         return screen.textContent = ""
-//     }
-
-// })
